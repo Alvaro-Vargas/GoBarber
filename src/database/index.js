3529993954138fd159afd77fe3cc3a9125cf4a1b
alvaro-vargas <alvaro.pvj@gmail.com>
2020-04-23 16:26:20 -0400

    Added feature: Appointments

--- src/database/index.js
+++ src/database/index.js
@@ -2,12 +2,13 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 // Import database configuration
 import databaseConfig from '../config/database';
 
 // Create an array with the imported models so we can loop through it.
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
