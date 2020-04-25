9e32676779495a09a44d206f4064acdf0386e944
alvaro-vargas <alvaro.pvj@gmail.com>
2020-04-24 22:04:30 -0400

    Added Feature: Notifications - Created mongoDB connection in databse Index - Created mongoDB Schema for notification - Created Notification Controller - Created routes: index and update for Notifications

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -13,6 +14,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -24,6 +26,17 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
